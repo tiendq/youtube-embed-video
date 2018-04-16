e15defc90cff15889c30abe86d8c21221c4736c1
Tien Do <tiendq@gmail.com>
2018-04-16 19:54:44 +0700

    Update example with class and style attributes

--- examples/app.js
+++ examples/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import YoutubeEmbedVideo from 'youtube-embed-video';
+import YoutubeEmbedVideo from '../dist/youtube';
+// import YoutubeEmbedVideo from 'youtube-embed-video';
 
-ReactDOM.render(<YoutubeEmbedVideo size="large" videoId="RnDC9MXSqCY" suggestions={false} />, document.getElementById("app"));
+ReactDOM.render(<YoutubeEmbedVideo size="medium" videoId="RnDC9MXSqCY" className="video-player" style={{ borderWidth: 5, borderColor: '#004ebc' }} suggestions={false} />, document.getElementById("app"));
