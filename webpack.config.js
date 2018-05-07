21bbc132e718ca8f2262fe96894109857d328d98
Tien Do <tiendq@gmail.com>
2018-05-07 09:29:19 +0700

    Upgrade to the latest packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
+let path = require('path');
+
 module.exports = {
   module: {
-    preLoaders: [{
+    rules: [{
+      enforce: 'pre',
       test: /\.js$/,
-      loader: "eslint",
+      use: 'eslint-loader',
       exclude: /node_modules/
-    }],
-    loaders: [{
+    }, {
       test: /\.js$/,
-      loader: "babel",
+      use: 'babel-loader',
       exclude: /node_modules/
     }]
   },
   entry: './src/youtube.js',
   output: {
-    filename: "youtube.js",
-    path: "./dist",
-    library: "YoutubeEmbedVideo",
-    libraryTarget: "commonjs2"
+    filename: 'youtube.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'YoutubeEmbedVideo',
+    libraryTarget: 'commonjs2'
   },
   externals: {
-    "prop-types": "prop-types",
-    "react": "react",
-    "react-dom": "react-dom"
+    'prop-types': 'prop-types',
+    'react': 'react',
+    'react-dom': 'react-dom'
   },
-  devtool: "source-map"
+  devtool: 'source-map'
 };
