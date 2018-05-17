f08a5e3df354fad501982c3230350fb45e44d5c0
Tien Do <tiendq@gmail.com>
2018-05-17 21:48:13 +0700

    test: use react-testing-library

--- src/youtube.test.js
+++ src/youtube.test.js
@@ -1,55 +1,30 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { render } from 'react-testing-library';
 import YoutubeEmbedVideo from "./youtube";
 
 const TEST_VIDEO_ID = 'RnDC9MXSqCY';
 
 test("Default settings, only videoId is required", () => {
-  const component = renderer.create(
-    <YoutubeEmbedVideo videoId={TEST_VIDEO_ID} />
-  );
-
-  let tree = component.toJSON();
-
-  expect(tree).toMatchSnapshot();
+  let { container } = render(<YoutubeEmbedVideo videoId={TEST_VIDEO_ID} />);
+  expect(container.firstChild).toMatchSnapshot();
 });
 
 test("Enhanced privacy mode and autoplay", () => {
-  const component = renderer.create(
-    <YoutubeEmbedVideo videoId={TEST_VIDEO_ID} enhancedPrivacy={true} autoplay={true} />
-  );
-
-  let tree = component.toJSON();
-
-  expect(tree).toMatchSnapshot();
+  let { container } = render(<YoutubeEmbedVideo videoId={TEST_VIDEO_ID} enhancedPrivacy={true} autoplay={true} />);
+  expect(container.firstChild).toMatchSnapshot();
 });
 
 test("Largest size without suggestions and media controls", () => {
-  const component = renderer.create(
-    <YoutubeEmbedVideo width={1280} height={720} videoId={TEST_VIDEO_ID} suggestions={false} controls={false} />
-  );
-
-  let tree = component.toJSON();
-
-  expect(tree).toMatchSnapshot();
+  let { container } = render(<YoutubeEmbedVideo width={1280} height={720} videoId={TEST_VIDEO_ID} suggestions={false} controls={false} />);
+  expect(container.firstChild).toMatchSnapshot();
 });
 
 test("Medium size and no show info", () => {
-  const component = renderer.create(
-    <YoutubeEmbedVideo videoId={TEST_VIDEO_ID} size="medium" showInfo={false} />
-  );
-
-  let tree = component.toJSON();
-
-  expect(tree).toMatchSnapshot();
+  let { container } = render(<YoutubeEmbedVideo videoId={TEST_VIDEO_ID} size="medium" showInfo={false} />);
+  expect(container.firstChild).toMatchSnapshot();
 });
 
 test("Customer style and class", () => {
-  const component = renderer.create(
-    <YoutubeEmbedVideo videoId={TEST_VIDEO_ID} className="video-player" style={{ borderWidth: 10, borderColor: '#00c775', borderStyle: 'solid' }} />
-  );
-
-  let tree = component.toJSON();
-
-  expect(tree).toMatchSnapshot();
+  let { container } = render(<YoutubeEmbedVideo videoId={TEST_VIDEO_ID} className="video-player" style={{ borderWidth: 10, borderColor: '#00c775', borderStyle: 'solid' }} />);
+  expect(container.firstChild).toMatchSnapshot();
 });
