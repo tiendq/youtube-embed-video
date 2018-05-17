18db6b0a2bf5ad88d71ef2d3046fb9fe966bd485
Tien Do <tiendq@gmail.com>
2018-05-17 21:09:05 +0700

    refactor: make test videoId constant

--- src/youtube.test.js
+++ src/youtube.test.js
@@ -2,9 +2,11 @@ import React from "react";
 import renderer from "react-test-renderer";
 import YoutubeEmbedVideo from "./youtube";
 
+const TEST_VIDEO_ID = 'RnDC9MXSqCY';
+
 test("Default settings, only videoId is required", () => {
   const component = renderer.create(
-    <YoutubeEmbedVideo videoId="RnDC9MXSqCY" />
+    <YoutubeEmbedVideo videoId={TEST_VIDEO_ID} />
   );
 
   let tree = component.toJSON();
@@ -14,7 +16,7 @@ test("Default settings, only videoId is required", () => {
 
 test("Enhanced privacy mode and autoplay", () => {
   const component = renderer.create(
-    <YoutubeEmbedVideo videoId="RnDC9MXSqCY" enhancedPrivacy={true} autoplay={true} />
+    <YoutubeEmbedVideo videoId={TEST_VIDEO_ID} enhancedPrivacy={true} autoplay={true} />
   );
 
   let tree = component.toJSON();
@@ -24,7 +26,7 @@ test("Enhanced privacy mode and autoplay", () => {
 
 test("Largest size without suggestions and media controls", () => {
   const component = renderer.create(
-    <YoutubeEmbedVideo width={1280} height={720} videoId="RnDC9MXSqCY" suggestions={false} controls={false} />
+    <YoutubeEmbedVideo width={1280} height={720} videoId={TEST_VIDEO_ID} suggestions={false} controls={false} />
   );
 
   let tree = component.toJSON();
@@ -34,7 +36,7 @@ test("Largest size without suggestions and media controls", () => {
 
 test("Medium size and no show info", () => {
   const component = renderer.create(
-    <YoutubeEmbedVideo videoId="RnDC9MXSqCY" size="medium" showInfo={false} />
+    <YoutubeEmbedVideo videoId={TEST_VIDEO_ID} size="medium" showInfo={false} />
   );
 
   let tree = component.toJSON();
@@ -44,7 +46,7 @@ test("Medium size and no show info", () => {
 
 test("Customer style and class", () => {
   const component = renderer.create(
-    <YoutubeEmbedVideo videoId="RnDC9MXSqCY" className="video-player" style={{ borderWidth: 10, borderColor: '#00c775', borderStyle: 'solid' }} />
+    <YoutubeEmbedVideo videoId={TEST_VIDEO_ID} className="video-player" style={{ borderWidth: 10, borderColor: '#00c775', borderStyle: 'solid' }} />
   );
 
   let tree = component.toJSON();
