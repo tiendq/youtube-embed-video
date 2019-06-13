74298ed9df7e7b4145c424789b914ae2cc0b1602
Tien Do <tiendq@gmail.com>
2019-06-13 14:31:28 +0700

    chore: upgrade to @babel and @testing-library packages

--- examples/app.js
+++ examples/app.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import YoutubeEmbedVideo from '../dist/youtube';
 // import YoutubeEmbedVideo from 'youtube-embed-video';
 
-ReactDOM.render(<YoutubeEmbedVideo size="medium" videoId="RnDC9MXSqCY" className="video-player" style={{ borderWidth: 10, borderColor: '#00c775', borderStyle: 'solid' }} suggestions={false} />, document.getElementById("app"));
+ReactDOM.render(<YoutubeEmbedVideo size="medium" videoId="RnDC9MXSqCY" className="video-player" style={{ borderWidth: 10, borderColor: '#00c775', borderStyle: 'solid' }} suggestions={false} />, document.getElementById('app'));
--- src/youtube.test.js
+++ src/youtube.test.js
@@ -1,30 +1,32 @@
-import React from "react";
-import { render } from 'react-testing-library';
-import YoutubeEmbedVideo from "./youtube";
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import YoutubeEmbedVideo from './youtube';
 
 const TEST_VIDEO_ID = 'RnDC9MXSqCY';
 
-test("Default settings, only videoId is required", () => {
+afterEach(cleanup);
+
+test('Default settings, only videoId is required', () => {
   let { container } = render(<YoutubeEmbedVideo videoId={TEST_VIDEO_ID} />);
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test("Enhanced privacy mode and autoplay", () => {
+test('Enhanced privacy mode and autoplay', () => {
   let { container } = render(<YoutubeEmbedVideo videoId={TEST_VIDEO_ID} enhancedPrivacy={true} autoplay={true} />);
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test("Largest size without suggestions and media controls", () => {
+test('Largest size without suggestions and media controls', () => {
   let { container } = render(<YoutubeEmbedVideo width={1280} height={720} videoId={TEST_VIDEO_ID} suggestions={false} controls={false} />);
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test("Medium size and no show info", () => {
+test('Medium size and no show info', () => {
   let { container } = render(<YoutubeEmbedVideo videoId={TEST_VIDEO_ID} size="medium" showInfo={false} />);
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test("Customer style and class", () => {
+test('Customer style and class', () => {
   let { container } = render(<YoutubeEmbedVideo videoId={TEST_VIDEO_ID} className="video-player" style={{ borderWidth: 10, borderColor: '#00c775', borderStyle: 'solid' }} />);
   expect(container.firstChild).toMatchSnapshot();
 });
